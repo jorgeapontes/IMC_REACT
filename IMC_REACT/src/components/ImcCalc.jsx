035e3f1ecf60488dbdba1235e28e90c912e90134
Jorge <jorgeappontes13@gmail.com>
2025-09-07 15:41:34 -0300

    cleat btn

--- IMC_REACT/src/components/ImcCalc.jsx
+++ IMC_REACT/src/components/ImcCalc.jsx
@@ -1,7 +1,35 @@
 import React from 'react'
 import './ImcCalc.css'
+import Button from './Button'
+import { useState } from 'react'
 
 const ImcCalc = () => {
+    const [height, setHeight] = useState("")
+    const [weight, setWeight] = useState("")
+    
+    const clearForm = (e) => {
+        e.preventDefault()
+        setHeight("")
+        setWeight("")
+    }
+
+     const validDigits = (text) => {
+        return text.replace(/[^0-9,]/g, '')
+
+    const handleHeightChange = (e) => {
+        const updatedValue = validDigits(e.target.value)
+        setHeight(updatedValue)
+    }
+
+
+    const handleWeightChange = (e) => {
+        setWeight(updatedValue)
+    }
+
+   
+
+    }
+
   return (
     <div id='calc-container'>
 
@@ -11,18 +39,22 @@ const ImcCalc = () => {
 
             <div className="form-control">
                 <label htmlFor="height">Altura</label>
-                <input type="text" name='height' id='height' placeholder='Exemplo: 1,75' />
+                <input type="text" name='height' id='height' placeholder='Exemplo: 1,75' 
+                onChange={(e) => handleHeightChange(e) }
+                value={height}/>
             </div>
             <div className="form-control">
                 <label htmlFor="weight">Peso</label>
-                <input type="text" name='weight' id='weight' placeholder='Exemplo: 80' />
+                <input type="text" name='weight' id='weight' placeholder='Exemplo: 80'
+                onChange={(e) => handleWeightChange(e) }
+                value={weight} />
             </div>
         
         </div>
 
         <div className="action-control">
-            <button>Calcular</button>
-            <button>Limpar</button>
+            <Button id="calc-btn" text="Calcular" />
+            <Button id="clear-btn" text="Limpar" action={clearForm} />
         </div>
     </form>
     </div>
